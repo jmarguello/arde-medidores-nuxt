cff291d40b73ddd5bce8356fdc93c31cb7c02629
Juan Manuel <jmarguello@gmail.com>
2022-03-31 22:01:23 -0300

    Sistema web de lectura de medidores

---
+++ layouts/default.vue
@@ -0,0 +1,10 @@
+<template>
+    <div>
+        <nav>Esta es la barra de navegacion</nav>
+
+
+        <nuxt/>
+
+
+    </div>
+</template>
---
+++ pages/index.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <nuxt-link :to="switchLocalePath('en')">English</nuxt-link>
+    <nuxt-link :to="switchLocalePath('es')">Español</nuxt-link>
+    <nuxt-link :to="switchLocalePath('fr')">Français</nuxt-link>
+    <br /><br />
+
+    <h1>{{ $t('hola') }}</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "IndexPage",
+};
+</script>
